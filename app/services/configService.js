eaee9fd67f650f9b49ae186c32d01f9d98da7a64
Ken Taylor <taylor.kenneth@gmail.com>
2015-06-01 23:56:06 -0400

    inital commit

---
+++ app/services/configService.js
@@ -0,0 +1,35 @@
+angular.module('gdgXBoomerang')
+.factory('Config', function () {
+    return {
+        // TODO Modify these to configure your app
+        'name'          : 'GDG Hampton Roads',
+        'id'            : '113573239350627723932',
+        'googleApi'     : 'AIzaSyDEK8ENpwwJUZmxXbbNW5EVjDZk7COG5-4',
+        'pwaId'         : '5915725140705884785', // Picasa Web Album id, must belong to Google+ id above
+        'domain'        : 'http://www.gdgspacecoast.org',
+        'twitter'       : 'gdg_hr',
+        'facebook'      : '',
+        'meetup'        : 'GDG-Hampton-Roads',
+        // Change to 'EEEE, MMMM d, y - H:mm' for 24 hour time format.
+        'dateFormat'    : 'EEEE, MMMM d, y - h:mm a',
+        'cover' : {
+            title: 'Worldwide GDG Events',
+            subtitle: 'Directory of developer events organized by tags and displayed on a global map.',
+            button: {
+                text: 'Find local events',
+                url: 'http://gdg.events/'
+            }
+        },
+        'activities': {
+            techTalks: true,
+            codeLabs: true,
+            hackathons: true,
+            devFests: true,
+            appClinics: true,
+            panels: true,
+            designSprints: true,
+            roundTables: true
+        }
+        // To update the snippet which is used for sharing, see the TODO in the index.html.
+    };
+});
